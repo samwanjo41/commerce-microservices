720df7400d657eb47e2880cf1effa44739d40bef
Samuel Wanjohi <samuel.wanjohi@tracom.co.ke>
2023-01-24 09:37:17 +0300

    added base skeleton for order service

--- product-service/product-service/src/main/java/com/samwanjohi/productservice/repository/ProductRepository.java
+++ product-service/product-service/src/main/java/com/samwanjohi/productservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.samwanjohi.productservice.repository;
 
 import com.samwanjohi.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
