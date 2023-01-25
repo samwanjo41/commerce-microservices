87cf6513bb179919a0acd4d39bc7c5726a9bb18a
Samuel Wanjohi <samuel.wanjohi@tracom.co.ke>
2023-01-25 15:18:16 +0300

    populate packages with relevant classes

---
+++ inventory-service/inventory-service/src/main/java/com/samwanjo/inventoryservice/model/Inventory.java
@@ -0,0 +1,21 @@
+package com.samwanjo.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_inventory")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
---
+++ inventory-service/inventory-service/src/main/java/com/samwanjo/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.samwanjo.inventoryservice.repository;
+
+import com.samwanjo.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
